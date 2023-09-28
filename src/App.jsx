45a3ed30c835e429c26855ec003e4839282c0569
Mateus <mateuscst45@gmail.com>
2023-09-28 19:51:33 -0300

    Started the app's structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Header from './Header.jsx'
+
+import './header.scss'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+function Header() {
+
+  return (
+    <>
+      <header>
+        <p>Made by Ktioru</p>
+        <h1>CV App</h1>
+
+        <h2>Dark Mode</h2>
+      </header>
+    </>
+  )
+}
+
+export default Header
